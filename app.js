a3b262bfc40e6665460a3da5fd99038a27a137f0
Charith <charith@botcalm.com>
2024-01-07 04:54:16 +0530

    function updated to server

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function add(a, b) {
-  return a + b;
-}
+// app.js
+const http = require("http");
+const add = require("./add");
 
-module.exports = add;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`Hello, Docker!\nResult of adding 5 and 7 = ${add(5, 7)}\n`);
+});
 
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
-// app.test.js
-const add = require("./app");
+// test/add.test.js
+const add = require("./add");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -9,8 +9,10 @@ test("adds 5 + 7 to equal 12", () => {
   expect(add(5, 7)).toBe(12);
 });
 
-test("adds 6 + 6 to not equal 12", () => {
-  expect(add(6, 6)).not.toBe(16);
+test("adds -1 + 5 to equal 4", () => {
+  expect(add(-1, 5)).toBe(4);
 });
 
-// Add more test cases as needed
+test("adds 0 + 0 to equal 0", () => {
+  expect(add(0, 0)).toBe(0);
+});
